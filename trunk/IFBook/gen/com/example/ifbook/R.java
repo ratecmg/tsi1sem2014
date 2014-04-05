16ec7ebd26c4e9d3ec6aa8d6903dda7a5905b972
ricardoborges.jr <ricardoborges.jr@7f7eb4cc-0781-be70-3b33-b094038b36bf>
2014-04-05 01:19:33 +0000

    adição das telas criação e login(Perfil)

--- trunk/IFBook/gen/com/example/ifbook/R.java
+++ trunk/IFBook/gen/com/example/ifbook/R.java
@@ -14,10 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TableLayout1=0x7f070000;
+        public static final int button1=0x7f070005;
+        public static final int editLogin=0x7f070001;
+        public static final int editSenha=0x7f070002;
+        public static final int menu_settings=0x7f070006;
+        public static final int textViewLogin=0x7f070003;
+        public static final int textViewSenha=0x7f070004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int perfil_criar=0x7f030000;
+        public static final int perfil_login=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
