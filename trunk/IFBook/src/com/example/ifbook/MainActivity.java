30e3c94f8024ae29c73bfdf39ba731496c19fd8e
smartzig <smartzig@7f7eb4cc-0781-be70-3b33-b094038b36bf>
2014-04-12 01:29:58 +0000

    atualização da mensagem

--- trunk/IFBook/src/com/example/ifbook/MainActivity.java
+++ trunk/IFBook/src/com/example/ifbook/MainActivity.java
@@ -3,13 +3,46 @@ package com.example.ifbook;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-
+	
+	
+	private Mensagem p;
+	private List<Mensagem> mensagem;
+	private MensagemDAO dao;
+	private EditText edID;
+	private EditText edTITULO;
+	private EditText edDESCRICAO;
+	private EditText edDATA;
+	private Spinner spCATEGORIA;
+	private EditText edIDUSUARIO;
+	private String operacao;
+	private String[] categoria = { "a" , "b" };
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.perfil_login);
+		
+		setContentView(R.layout.main);
+		edID = (EditText) findViewById(R.id.edID);
+		edTITULO = (EditText) findViewById(R.id.edTITULO);
+		edDESCRICAO = (EditText) findViewById(R.id.edDESCRICAO);
+		edDATA = (EditText) findViewById(R.id.edDATA);
+		spCATEGORIA = (Spinner) findViewById(R.id.spCATEGORIA);
+		edIDUSUARIO = (EditText) findViewById(R.id.edIDUSUARIO);
+		mensagem = new ArrayList<Mensagem>;
+		operacao = new String ("Novo");
+		dao = new MensagemDAO(getApplicationContext());
+		preencherCategoria();
+		atualizarLista();
+		
+		
 	}
 
 	@Override
@@ -19,4 +52,58 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private void preencherCategoria(){
+		try{
+			ArrayAdapter<String> adapter =
+					new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, categoria);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			spCATEGORIA.setAdapter(adapter);
+		} catch (Exception ex){
+			exibirMensagem("Erro:" + ex.getMessage());
+		}
+		
+	}
+	
+	public void enviar(View v){
+		
+		if (operacao.equalsIgnoreCase("Novo")) {
+			p = new Mensagem();
+	
+		}
+		
+		p.setTitulo(edTITULO.getText().toString());
+		p.setDescricao(edDESCRICAO.getText().toString());
+		p.setCategoria(categoria[spCATEGORIA.getSelectedItemPosition()]
+				.equalsIgnoreCase("a") ? "a" : "b");
+		
+		p.setData(edDATA.getText().toString());
+		p.setIdUsuario(edIDUSUARIO.getText().toString());
+		limparDados();
+		
+	}
+	
+	public void novo(View v) {
+		operacao=new String("Novo");
+		limparDados();
+	}
+	
+	private void limparDados() {
+		edID.setText("");
+		edTITULO.requestFocus();
+		edDESCRICAO.setText("");
+		edDATA.setText("");
+		spCATEGORIA.setSelection(0);
+		edIDUSUARIO.setText("");
+		
+	}
+	
+	private void preencherDados(Mensagem mensagem) {
+		edID.setText(String.valueOf(Mensagem.getId()));
+		edTITULO.setText(mensagem.getTitulo);
+		edDESCRICAO.setText(mensagem.getDescricao);
+		edDATA.setText(String.valueOf(Mensagem.getId()));
+		spCATEGORIA.setSelection(mensagem.getCategoria().equalsIgnoreCas("a") ? 0 : 1);
+		edIDUSUARIO.setText(String.valueOf(Mensagem.getIdUsuario()));
+	}
+	
 }
