733f6f96c9ef6f68b049ff61baad34a69ab58bbe
smartzig <smartzig@7f7eb4cc-0781-be70-3b33-b094038b36bf>
2014-05-24 00:02:01 +0000

    CRIANDO ESTRTURA INICIAL

---
+++ IFBookWebService-ServidorRest/src/br/edu/ifsp/ddm/exemplocadastropessoa/ws/servidor/modelo/Usuario.java
@@ -0,0 +1,111 @@
+package br.edu.ifsp.ddm.exemplocadastropessoa.ws.servidor.modelo;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4360718276063483351L;
+	private int idUsuario;
+	private String Prontuario;
+	private String Senha;
+	private int Nivel;
+	private String Nome;
+	private String Apelido;
+	private String Nascimento;
+	private String LocalTrabalho;
+	private String Cidade;
+	private String Email;
+	private String Telefone;
+	private byte[] Foto = null;
+	private EstadoCivil idEstadoCivil;
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getProntuario() {
+		return Prontuario;
+	}
+	public void setProntuario(String prontuario) {
+		Prontuario = prontuario;
+	}
+	public String getSenha() {
+		return Senha;
+	}
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+	public int getNivel() {
+		return Nivel;
+	}
+	public void setNivel(int nivel) {
+		Nivel = nivel;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getApelido() {
+		return Apelido;
+	}
+	public void setApelido(String apelido) {
+		Apelido = apelido;
+	}
+	public String getNascimento() {
+		return Nascimento;
+	}
+	public void setNascimento(String nascimento) {
+		Nascimento = nascimento;
+	}
+	public String getLocalTrabalho() {
+		return LocalTrabalho;
+	}
+	public void setLocalTrabalho(String localTrabalho) {
+		LocalTrabalho = localTrabalho;
+	}
+	public String getCidade() {
+		return Cidade;
+	}
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(String telefone) {
+		Telefone = telefone;
+	}
+	public byte[] getFoto() {
+		return Foto;
+	}
+	public void setFoto(byte[] foto) {
+		Foto = foto;
+	}
+	public EstadoCivil getIdEstadoCivil() {
+		return idEstadoCivil;
+	}
+	public void setIdEstadoCivil(EstadoCivil idEstadoCivil) {
+		this.idEstadoCivil = idEstadoCivil;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [idUsuario=" + idUsuario + ", Prontuario=" + Prontuario
+				+ ", Senha=" + Senha + ", Nivel=" + Nivel + ", Nome=" + Nome
+				+ ", Apelido=" + Apelido + ", Nascimento=" + Nascimento
+				+ ", LocalTrabalho=" + LocalTrabalho + ", Cidade=" + Cidade
+				+ ", Email=" + Email + ", Telefone=" + Telefone + ", Foto="
+				+ Foto + ", idEstadoCivil=" + idEstadoCivil + "]";
+	}
+}
